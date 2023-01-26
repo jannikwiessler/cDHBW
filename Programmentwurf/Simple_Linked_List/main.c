d052f4738554ea325529e656d706b2563df2adb8
StuBITtv <29468630+stubittv@users.noreply.github.com>
2023-01-26 16:43:52 +0100

    apply consistent formatting

--- Programmentwurf/Simple_Linked_List/main.c
+++ Programmentwurf/Simple_Linked_List/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "linkedListLib.h"
+
 /*******************************************************
 * NAME: main.c (main) 
 *
@@ -29,43 +30,62 @@
 *   >> Practice of simple linked list
 *
 **/
-int main(){
-
+int main() {
     listElement *start;
-    start = (listElement *)malloc(sizeof(listElement));
+    start = (listElement *) malloc(sizeof(listElement));
+
     if (start == NULL) {
-        printf("can't reserve storage.\n"); 
+        printf("can't reserve storage.\n");
         return -1;
-        }
+    }
+
     start->nextElem = NULL;
 
     int FLAGG = 1;
-    while (FLAGG){
-    
+    while (FLAGG) {
         printf("1... print list\n");
         printf("2... add element\n");
         printf("3... delete element\n");
         printf("4... delete list\n");
-		printf("5... save list\n");
-		printf("6... load list\n");
-		printf("7... sort list\n");
+        printf("5... save list\n");
+        printf("6... load list\n");
+        printf("7... sort list\n");
         printf("0... quit\n");
-        scanf("%d",&FLAGG);
+        scanf("%d", &FLAGG);
         system("cls"); // clear for mac/linux
 
-
-        switch (FLAGG){
-            case 1: printList(start); break;
-            case 2: addListElem(start); break;
-            case 3: delListElem(start); break;
-            case 4: delList(start); break;
-			case 5: saveList(start); break;
-			case 6: loadList(start); break;
-            case 7: sortList(start); break;
-            case 0: FLAGG = 0; exitFcn(start); break;
-            default: printf("invalid choice\n"); break;
+        switch (FLAGG) {
+            case 1:
+                printList(start);
+                break;
+            case 2:
+                addListElem(start);
+                break;
+            case 3:
+                delListElem(start);
+                break;
+            case 4:
+                delList(start);
+                break;
+            case 5:
+                saveList(start);
+                break;
+            case 6:
+                loadList(start);
+                break;
+            case 7:
+                sortList(start);
+                break;
+            case 0:
+                FLAGG = 0;
+                exitFcn(start);
+                break;
+            default:
+                printf("invalid choice\n");
+                break;
         }
     }
+
     return 0;
 }
 
